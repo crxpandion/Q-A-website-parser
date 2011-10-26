eb7c1ee2dba6f67b412265307c10cc8fac0fba64
Kai Hayashi <k.hermitian@gmail.com>
2011-10-26 18:29:43 -0500

    added ability to convert url to a xml tree structure

--- QAParser/parser.py
+++ QAParser/parser.py
@@ -1,4 +1,14 @@
 import urllib2
+from xml.dom.minidom import parse
 
+def makeDOM(url):
+  dom = urllib2.urlopen(url).read()
+  return parse(dom)
+  
+def getQuestion(DOM):
+  return 'not implemented'
 
+def getAnswer(DOM):
+  return 'not implemented'
 
+print makeDOM('http://www.google.com')

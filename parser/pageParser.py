50805069b67d5b7c682a90569c47b455dacfa0b1
Kai Hayashi <k.hermitian@gmail.com>
2011-10-27 16:51:37 -0500

    added run method to pageParser class. created skeletons for the DB insertion functions

--- parser/pageParser.py
+++ parser/pageParser.py
@@ -3,6 +3,12 @@ import urllib2
 from bs4 import BeautifulSoup
 
 class parsePage:
+  class subClassError(Exception):
+    def __init__(self, value):
+      self.value = value
+    def __str__(self):
+      return repr(self.value)
+
   def __init__(self, url):
     self.url = url
     self.dom = self.makeDOM()
@@ -10,10 +16,23 @@ class parsePage:
   def makeDOM(self):
     return BeautifulSoup(urllib2.urlopen(self.url).read())
   
+  def run(self):
+    self.question = self.getQuestion()
+    self.answer = self.getAnswer()
+    self.insertQuestion()
+    self.insertAnswer()
+
+  # insert stuff into DB 
+  def insertQuestion(self):
+    print 'insertQuestion is not implemented yet'
+
+  def insertAnswer(self):
+    print 'insertAnswer is not implemented yet'
+
   # these will be overloaded in the modules 
   def getQuestion(self):
-    print 'not implemented'
+    raise subClassError('getQuestion is not implemented')
 
   def getAnswer(self):
-    print 'not implemented'
+    raise subClassError('getAnswer is not implemented')
 

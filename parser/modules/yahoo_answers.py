a9d2e4c490a73728c3605bdd613a2bf48d5835b6
Daniel Myers <Dmritard96@gmail.com>
2011-10-27 17:43:17 -0500

    added yahoo getAnswers

--- parser/modules/yahoo_answers.py
+++ parser/modules/yahoo_answers.py
@@ -1,18 +1,25 @@
 #!/usr/bin/python
+import sys
+sys.path.append('..')
 from bs4 import BeautifulSoup
-from ..pageParser import parsePage 
+from pageParser import parsePage 
 
 class parseQAPage(parsePage):
   def getQuestion(self):
     question_field = self.dom.find('div', id='yan-question')
-    question_title = question_field.fint('h1', 'subject')
-    question_test = question_field.find('div', 'content')
-    print question_test
-    print question_title
+    title = question_field.find('h1', 'subject')
+    question = question_field.find('div', 'content')
+    #insertQuestion(...)
 
   def getAnswer(self):
-    print 'not implemented yet'
+    answers = self.dom.findAll( 'div', 'answer')
+    for answer in answers:
+        time = answer.find('abbr')
+        content = answer.find('div', 'content')
+        rating = answer.find('span', 'seo-rated')
+        #insertAnswer(...)
 
 # small tests - we should really get a testing package going...
 pp = parseQAPage('http://answers.yahoo.com/question/index;_ylt=Ai47Sv1EzW5EmuoBQIq9m.Oe5HNG;_ylv=3?qid=20110907120254AALqqeS')
 pp.getQuestion()
+pp.getAnswer()

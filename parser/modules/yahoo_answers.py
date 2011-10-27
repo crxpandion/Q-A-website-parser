f177a352243cf090bc9032f87606a1a0e9d76b09
Kai Hayashi <k.hermitian@gmail.com>
2011-10-27 18:02:47 -0500

    modified the getanswers/questionsto return dicitonarys or arrays of dictionarys

--- parser/modules/yahoo_answers.py
+++ parser/modules/yahoo_answers.py
@@ -6,16 +6,31 @@ from pageParser import parsePage
 
 class parseQAPage(parsePage):
   def getQuestion(self):
+    q = {}
+    user = {}
     question_field = self.dom.find('div', id='yan-question')
-    title = question_field.find('h1', 'subject')
-    question = question_field.find('div', 'content')
+    q['title'] = question_field.find('h1', 'subject')
+    q['question'] = question_field.find('div', 'content')
+    # user['name']
+    # user['stats']
+    q['user'] = user
+    return q
 
   def getAnswer(self):
     answers = self.dom.findAll( 'div', 'answer')
+    a = []
     for answer in answers:
-        time = answer.find('abbr')
-        content = answer.find('div', 'content')
-        rating = answer.find('span', 'seo-rated')
+        user = {}
+        ans = {}
+        ans['time'] = answer.find('abbr')
+        ans['content'] = answer.find('div', 'content')
+        ans['rating'] = answer.find('span', 'seo-rated')
+        # user['name']
+        # user['stats']
+        ans['user'] = user
+        a.append(ans)
+    return a
+
 
 # small tests - we should really get a testing package going...
 pp = parseQAPage('http://answers.yahoo.com/question/index;_ylt=Ai47Sv1EzW5EmuoBQIq9m.Oe5HNG;_ylv=3?qid=20110907120254AALqqeS')

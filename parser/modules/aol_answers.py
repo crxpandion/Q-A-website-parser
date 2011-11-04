691b62060c445e451db69c844291ffe6bc2b15ba
Daniel Myers <Dmritard96@gmail.com>
2011-11-04 12:30:21 -0500

    refined the answers grabbing for aol answers

--- parser/modules/aol_answers.py
+++ parser/modules/aol_answers.py
@@ -3,6 +3,7 @@ import sys
 sys.path.append('..')
 from bs4 import BeautifulSoup
 from pageParser import parsePage 
+import datetime
 
 class parseQAPage(parsePage):
     def getQuestion(self):
@@ -10,7 +11,7 @@ class parseQAPage(parsePage):
         user = {}
         question_field = self.dom.find('div', id='QuestionBlogPostDetails')
         content = question_field.find('div', 'Content')
-        q['datetime'] = question_field.find('span', 'PostedOn').string
+        q['datetime'] = getPostDate(question_field.find('span', 'PostedOn').string)
         q['question_text'] = content.find('h1').contents[0].lstrip().rstrip()
         q['user'] = content.find('a', attrs={'title':True})
         return q
@@ -21,9 +22,14 @@ class parseQAPage(parsePage):
         for answer in answers:
             user = {}
             ans = {}
-            ans['datetime'] = answer.find('a', 'PostedOn')
-            ans['answer'] = answer.find('p')
-            ans['upVotes'] = answer.find('span', 'QBRankVoteHeader')
+            ans['datetime'] = getPostDate(answer.find('a', 'PostedOn').string)
+            ans['answer'] = answer.find('p').string
+            ans['upVotes'] = answer.find('span', 'QBRankVoteHeader').contents[0][1]
             user['user'] = answer.find('a', attrs={'title':True})
             a.append(ans)
+        print a[0]
         return a
+
+def getPostDate(postedOn):
+    return str(datetime.datetime.now() - datetime.timedelta(days=int(postedOn.split()[1]))).split()[0] + ' 00:00:00'
+

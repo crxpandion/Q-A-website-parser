ddb4527332186bccfd984b00aa22694de7a0d199
Kai Hayashi <k.hermitian@gmail.com>
2011-11-25 17:13:30 -0600

    cleaned up

--- parser/QA_runner.py
+++ parser/QA_runner.py
@@ -2,18 +2,22 @@
 import sys, urllib2, re
 sys.path.append('modules')
 
-class QA_runner():
-  class ParserError(Exception):
-    def __init__(self, m):
-      self.m = m
+class ParserError(Exception):
+    def __init__(self, value):
+      self.value = value
     def __str__(self):
-      return repr(self.m)
+      return repr(self.value)
 
+class QA_runner():
   def __init__(self, path, verbose = False):
     self.path = path
     self.verbose = verbose
-    self.parser = self.choose_parser(path)
-    self.run()
+    try:
+      self.parser = self.choose_parser(path)
+      self.run()
+    except ParserError, m:
+      if self.verbose:
+        print m.value
 
   def choose_parser(self, path):
     def open_website(path):
@@ -24,17 +28,16 @@ class QA_runner():
       return f.read() 
     self.html = open_website(path)
     if 'on AOL Answers' in self.html:
-      import aol_answers as parser
+      import aol_answers as p
     elif 'Yahoo! Answers' in self.html:
-      import yahoo_answers as parser
+      import yahoo_answers as p
     elif 'Answer.Ask.com' in self.html:
-      import ask_answers as parser
+      import ask_answers as p
     elif 'Search Askville' in self.html:
-      import askville_answers as parser
+      import askville_answers as p
     else:
-      if self.verbose:
-        raise parserError('Unrecognized site')
-    return parser.parseQAPage(self.html, self.verbose)
+      raise ParserError('Error: could not recognize site')
+    return p.parseQAPage(self.html, self.verbose)
 
   def run(self):
     self.parser.run() 
@@ -45,8 +48,8 @@ QA_runner('http://askville.amazon.com/formation-sun-dog/AnswerViewer.do?requestI
 QA_runner('http://aolanswers.com/questions/poor_people_better_today_four_years_735670198934250', False)
 QA_runner('http://answers.ask.com/Science/Other/how_big_is_the_sun', False)
 QA_runner('http://answers.yahoo.com/question/index;_ylt=Ai47Sv1EzW5EmuoBQIq9m.Oe5HNG;_ylv=3?qid=20110907120254AALqqeS', False)
-QA_runner('http://askville.amazon.com/long-sun-Vitamin-day/AnswerViewer.do?requestId=76403763', False)
-QA_runner('http://www.facebook.com')
+QA_runner('http://askville.amazon.com/long-sun-Vitamin-day/AnswerViewer.do?requestId=76403763', True)
+QA_runner('http://www.facebook.com', True)
 
 #Old URLs, should go back and try these
 #print parseQAPage(urllib2.urlopen('http://aolanswers.com/questions/poor_people_better_today_four_years_735670198934250').read()).getQuestion()

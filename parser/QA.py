b0800f38090a668e297d7f726f826431339fd05d
Daniel Myers <Dmritard96@gmail.com>
2011-11-24 22:28:05 -0600

    added a few more URLS to test in QA.py

--- parser/QA.py
+++ parser/QA.py
@@ -26,10 +26,12 @@ class QA():
          	    print 'Site is not recognized'
 
 # When this project is finished this will not be here...handy for debugging though
+QA(urllib2.urlopen('http://askville.amazon.com/sheryl-crow-version-song-sun/AnswerViewer.do?requestId=8804279').read(), True)
+QA(urllib2.urlopen('http://askville.amazon.com/formation-sun-dog/AnswerViewer.do?requestId=5657915').read(), True)
 QA(urllib2.urlopen('http://aolanswers.com/questions/poor_people_better_today_four_years_735670198934250').read(), False)
 QA(urllib2.urlopen('http://answers.ask.com/Science/Other/how_big_is_the_sun').read(), False)
-QA(urllib2.urlopen('http://answers.yahoo.com/question/index;_ylt=Ai47Sv1EzW5EmuoBQIq9m.Oe5HNG;_ylv=3?qid=20110907120254AALqqeS').read(), True)
-QA(urllib2.urlopen('http://askville.amazon.com/long-sun-Vitamin-day/AnswerViewer.do?requestId=76403763').read(), True)
+QA(urllib2.urlopen('http://answers.yahoo.com/question/index;_ylt=Ai47Sv1EzW5EmuoBQIq9m.Oe5HNG;_ylv=3?qid=20110907120254AALqqeS').read(), False)
+QA(urllib2.urlopen('http://askville.amazon.com/long-sun-Vitamin-day/AnswerViewer.do?requestId=76403763').read(), False)
 QA(urllib2.urlopen('http://www.facebook.com').read())
 
 #Old URLs, should go back and try these

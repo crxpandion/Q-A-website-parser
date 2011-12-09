0b0ef9f20c88d50ca4048ed8d73c77571319ac3a
Daniel Myers <Dmritard96@gmail.com>
2011-12-09 08:38:12 -0600

    incorporated jesses changes

--- parser/modules/ask_answers.py
+++ parser/modules/ask_answers.py
@@ -8,22 +8,46 @@ import datetime
 class parseQAPage(parsePage):
     def getQuestion(self):
         q = {}
-        question_field = self.dom.find('div', id='qnaQuesComm')
-        q['question_text'] = question_field.find('div', 'txt_xxlg abstract pl15 pb10').contents[0].lstrip().rstrip()
-        q['user'] = question_field.find('div', 'rr_life_title b').contents[0].replace(' asked','')
+	try:
+        	question_field = self.dom.find('div', id='qnaQuesComm')
+        	q['question_text'] = question_field.find('div', 'txt_xxlg abstract pl15 pb10').contents[0].lstrip().rstrip()
+	except:
+		q['question_text'] = "N/A"
+	try:
+        	q['user'] = question_field.find('div', 'rr_life_title b').contents[0].replace(' asked','')
+	except:
+		q['user'] = "N/A"
         q['datetime'] = 'not_given'
         return q
         
     def getAnswers(self):
         a = []
-        answers = self.dom.findAll('div', 'ansbrd')[1:]
+	try:
+        	answers = self.dom.findAll('div', 'ansbrd')[1:]
+	except:
+		answers = "N/A"
         for answer in answers:
             ans = {}
-            info = answer.findAll('span')
-            ans['user'] = info[0].string.replace(':', '')
-            ans['answer'] = info[1].string
-            ans['datetime'] = getPostDate(answer.find('div', 'clrg pt10').string)
-            ans['upVotes'] =  answer.find('div', 'fl pr15 ml5').string
+	    try:
+                info = answer.findAll('span')
+	    except:
+		info = "N/A"
+	    try:
+            	ans['user'] = info[0].string.replace(':', '')
+	    except:
+		ans['user'] = "N/A"
+	    try:
+            	ans['answer'] = info[1].string
+	    except:
+		ans['answer'] = "N/A"
+	    try:
+            	ans['datetime'] = getPostDate(answer.find('div', 'clrg pt10').string)
+	    except:
+		ans['datetime'] = "1970-01-01 00:00:00"
+	    try:
+            	ans['upVotes'] =  answer.find('div', 'fl pr15 ml5').string
+	    except:
+		ans['upVotes'] = '0'
             a.append(ans)
         return a
 		
